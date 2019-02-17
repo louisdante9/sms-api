3a49b8c28c00619b3cf0cc73faa00c029ea582b8
Louis Nwamadi <louisdante9@gmail.com>
2019-02-17 17:13:05 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import logger from 'morgan';
+import routes from './routes';
+
+const app = express();
+const PORT = process.env.PORT || 3002
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// app.use('/api', routes);
+app.get('/', (req, res)=> {
+    res.send('hello there')
+})
+
+app.use('*', (req, res) => res.status(404).json({ message: 'Requested path not configured' }));
+
+app.listen(PORT, () => console.log('app running PORT: ', PORT));
+
+export default app;
